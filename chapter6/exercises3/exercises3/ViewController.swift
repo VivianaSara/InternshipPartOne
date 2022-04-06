056bb2fdeef0273bee9c3a4d6a49891b64e65718
VivianaSara <viviana.mesaros@fortech.ro>
2022-04-06 16:50:29 +0300

    first commit

---
+++ chapter6/exercises3/exercises3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercises3
+//
+//  Created by Viviana Mesaros on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
